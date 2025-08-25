aa6cfe14a3c088d40d6477ff6efe0e3344a5cb64
Bogdan224 <kolganov_25@mail.ru>
2025-08-26 00:16:19 +0700

    -add showing services

---
+++ RestarauntWebApp/Controllers/ServicesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestarauntWebApp.Domain;
+using RestarauntWebApp.Domain.Entities;
+using RestarauntWebApp.Infrastructure;
+using RestarauntWebApp.Models;
+
+namespace RestarauntWebApp.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager _dataManager;
+
+        public ServicesController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(HelperDTO.TransformServices(await _dataManager.Services.GetServicesAsync()));
+        }
+
+        public async Task<IActionResult> Show(int id)
+        {
+            Service? entity = await _dataManager.Services.GetServiceByIdAsync(id);
+
+            if (entity is null) return NotFound();
+
+            return View(HelperDTO.TransformService(entity));
+        }
+    }
+}
